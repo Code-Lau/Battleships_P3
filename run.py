c0be1c7b1c87a6838411852880f2d8be94d7495d
Code-Lau <laurenleighwoods@gmail.com>
2024-10-27 19:47:10 +0000

    Add CIPL changes

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-#How to Code Battleship in Python (https://www.youtube.com/watch?v=tF1WRCrd_HQ)
+# How to Code B.S. in Python (https://www.youtube.com/watch?v=tF1WRCrd_HQ)
 from random import randint
 import os
 
-#Board for the ship locations
+# Board for the ship locations
 HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
 # Board for tracking hits/misses
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
+
 
 def welcome():
     os.system('clear')
@@ -15,6 +16,7 @@ def welcome():
     print("If a ship is hit, an 'X' will be shown.")
     print("If you miss, a '-' will be shown.")
 
+
 def print_board(board):
     print("  A B C D E F G H")
     print("  +-+-+-+-+-+-+-+")
@@ -22,6 +24,7 @@ def print_board(board):
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
+
 
 letters_to_numbers = {
     'A': 0,
@@ -34,15 +37,21 @@ letters_to_numbers = {
     'H': 7
 }
 
-#Creates the ships
+# Creates the ships
+
+
 def create_ships(board):
     for ship in range(5):
-        ship_row, ship_column = randint(0,7), randint(0,7)
+        ship_row,
+        ship_column = randint(0, 7),
+        randint(0, 7)
         while board[ship_row][ship_column] == "X":
             ship_row, ship_column = get_ship_location()
         board[ship_row][ship_column] = "X"
 
-#Gets the ship location
+# Gets the ship location
+
+
 def get_ship_location():
     row = input("Enter the row of the ship: ").upper()
     while row not in "12345678":
@@ -54,18 +63,18 @@ def get_ship_location():
         column = input("Enter the column of the ship: ").upper()
     return int(row) - 1, letters_to_numbers[column]
 
-#Checks if ships are hit
+# Checks if ships are hit
+
+
 def count_hit_ships(board):
     count = 0
     for row in board:
         for column in row:
             if column == "X":
                 count += 1
-    return count
-
-if __name__ == "__main__":
-    create_ships(HIDDEN_BOARD)
-    turns = 10
+    return count if __name__ == "__main__":
+        create_ships(HIDDEN_BOARD)
+        turns = 10
     while turns > 0:
         print('Guess a battleship location.')
         print_board(GUESS_BOARD)
@@ -74,12 +83,12 @@ if __name__ == "__main__":
             print("This spot has been checked already.")
         elif HIDDEN_BOARD[row][column] == "X":
             print("Hit")
-            GUESS_BOARD[row][column] = "X" 
-            turns -= 1  
+            GUESS_BOARD[row][column] = "X"
+            turns -= 1
         else:
             print("MISS!")
-            GUESS_BOARD[row][column] = "-"   
-            turns -= 1     
+            GUESS_BOARD[row][column] = "-"
+            turns -= 1
         if count_hit_ships(GUESS_BOARD) == 5:
             print("You win!")
             break
